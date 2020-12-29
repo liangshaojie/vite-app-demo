942964e99ba53b5604e76fadaa4f579605246af7
liangshaojie <877942720@qq.com>
2020-12-29 22:31:05 +0800

    去掉4545

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p class="testModule">Edit000 <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p class="testModule">Edit000 <code>components/HelloWorld.vue</code> t
+  o test hot module replacement.</p>
 </template>
 
 <script>
