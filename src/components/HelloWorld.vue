65569a4871242f832a7af75673d8d05f26312ae8
liangshaojie <877942720@qq.com>
2020-12-29 21:55:46 +0800

    scss配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p :class="$style.testModule">Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p class="testModule">Edit000 <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
 <script>
@@ -18,8 +18,9 @@ export default {
 }
 </script>
 
-<style module>
+<style lang="scss">
+/* use scss */
 .testModule{
-  color:red;
+  color:pink;
 }
 </style>
